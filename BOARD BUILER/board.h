00a6d5ba615f94fa6419368e0008ddf07a18c8ec
RuiMoreira1 <57903845+RuiMoreira1@users.noreply.github.com>
2020-05-06 23:01:08 +0100

    Update board.h

--- BOARD BUILER/board.h
+++ BOARD BUILER/board.h
@@ -22,22 +22,19 @@ public:
         rows = 0; columns = 0;
         row_coord = 0; column_coord = 0;
         row_board = 65; column_board = 97;
-        direction = "", input_Word = "abaci";
+        direction = "", input_Word = "";
     }
-    void test();
     void vector_init(); //Initialize the vector that holds the matrix
     void FillCord();  //Fills the first column and row with "coordinates"
     void PrintBoard() const; //Prints the Board
     void read_file();
     void scan_input_cord();
     void word_in_vec();
-    void word_insert_horizontal();
-    void word_insert_vertical();
     void insert_to_file();
     void word_insert_horizontal_2();
     void ending_file();
     void word_insert_vertical_2();
     void print_file();
-    bool direc();
+    bool direct();
     static bool end();
 };
