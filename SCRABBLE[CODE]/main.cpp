d81aa2f54ccee355d174d7ca6daa06de5c7389c7
RuiMoreira1 <57903845+RuiMoreira1@users.noreply.github.com>
2020-05-06 20:44:09 +0100

    Update main.cpp

--- SCRABBLE[CODE]/main.cpp
+++ SCRABBLE[CODE]/main.cpp
@@ -4,18 +4,20 @@
 using namespace std;
 
 
+
 // NOT REAL MAIN JUST TESTING IF THE CREATED FUNCTION WORK!!!
 int main() {
     BOARD nice;
     nice.open_file();
     nice.board_input_init();
     nice.split_string();
-    nice.test();
     nice.board_size();
     nice.storage_init();
     nice.Fill_Cord();
     nice.Print_Board();
     nice.Build_Board();
-    nice.Print_Board();
+    nice.vectors();
+
 
 }
+
