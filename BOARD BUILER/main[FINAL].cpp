8e5a7856dedfd7137d3c614f5b50206ac17c5cbb
RuiMoreira1 <57903845+RuiMoreira1@users.noreply.github.com>
2020-05-20 10:34:50 +0100

    Update main[FINAL].cpp

--- BOARD BUILER/main[FINAL].cpp
+++ BOARD BUILER/main[FINAL].cpp
@@ -1,33 +1,29 @@
 #include "board.h"
-using namespace std;
-
 
 
 
 int main(){
-    Board var;
-    var.scan_input_board();
-    var.vector_init();
-    var.FillCord();
-    var.PrintBoard();
-    var.read_file();
+    auto *var = new Board(Board::Get_warnings(3));
+    var->FillCord();
+    var->PrintBoard();
+    while(!var->read_file(Board::Get_warnings(4)));                 //If the file can´t be opened it will throw an exception, until the file can be opened
     while(true){
-        var.word_in_vec();
-        while(true) {
-            var.scan_input_cord();
-            if (var.Get_direction()) {
-                if(!var.word_insert_horizontal_2())var.Error();
-                else break;
-            }
-            else{
-                if(!var.word_insert_vertical_2()) var.Error();
-                else break;
-            }
-        }
-        var.PrintBoard();
-        if(Board::end()) break;
+        var->word_in_vec();
+        var->scan_input_cord(Board::Get_warnings(0),Board::Get_warnings(2));
+        while((var->Get_direction()) ? !var->word_insert_horizontal_2(Board::Get_warnings(1)) : !var->word_insert_vertical_2(Board::Get_warnings(1))) {
+            var->scan_input_cord(Board::Get_warnings(0),Board::Get_warnings(2));
+            var->Error();                                                                       //If the word doesn't fit the board
+        }                                                                                       //preserve the word, and ask for the
+        var->PrintBoard();                                                                      //the user input(Direction/Row/Column) again
+        if(Board::end()) break;                                                                 //until the insertion of the word is valid
+        else continue;
     }
-
-    var.ending_file();
-
+    var->ending_file(Board::Get_warnings(4));
+    delete var;
+    return 0;
 }
+
+
+
+
+//CREATED BY RUI_
