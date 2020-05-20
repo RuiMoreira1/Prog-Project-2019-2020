0ed20d72363c557360f0fea613b95ae8fc1f0cf0
RuiMoreira1 <57903845+RuiMoreira1@users.noreply.github.com>
2020-05-20 11:25:15 +0100

    Update BOARD.cpp

--- SCRABBLE[CODE]/BOARD.cpp
+++ SCRABBLE[CODE]/BOARD.cpp
@@ -4,33 +4,53 @@ using namespace std;
 
 
 
-void text_color(string text, int color) {
+void text_color(const string &text, int color) {
     HANDLE hConsole;
     hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
     SetConsoleTextAttribute(hConsole, color);
     cout << text;
 }
 
+
+//Constructor
+BOARD::BOARD(const Warnings &war){
+    while(!open_file(war));
+    board_input_init();
+    split_string();
+    board_size();
+    storage_init();
+    Fill_Cord();
+    Build_Board();
+    matrix_copy();
+    vectors();
+    Print_Board();
+}
+
+//Input errors, File errors
+Warnings BOARD::Get_Warnings(int i) {
+    string file_error = "Error opening file!\n";
+    vector<Warnings> Warning = {file_error};
+    return Warning[i];
+}
+
 //Reading file line by line and inserting each line into a vector, if the file can´t be opened it throws an exception!!!
-//TENS DE ALTERAR O PATH DO TEU FICHEIRO !!!!!!!!
-void BOARD::open_file() {
-    string line;
-    string user_file;
-    cout << "Board information file location:" << endl;
+bool BOARD::open_file(const Warnings &war) {
+    string line, user_file;
+    cout << "Board information full file location:" << endl;
     cin >> user_file;
-    try {
+    try{
         fstream file(user_file.c_str(), ios::in);
-        if (file.is_open()) {
-            while (getline(file, line)) {
+        if(file.is_open()){
+            while(getline(file,line)){
                 file_input.push_back(line);
             }
         }
-        else throw ios::failure("Error opening file!\n");
-        file.close();
+        else throw ios::failure(war);
+        file.close(); return true;
     }
-    catch (const exception & ex) {
+    catch(const exception &ex){
         cerr << ex.what() << endl;
-        exit(1);
+        cin.clear(); cin.ignore(10000000,'\n'); return false;
     }
 }
 
@@ -77,14 +97,12 @@ void BOARD::matrix_copy() {
 
 //FILL THE ROW 0 AND COL 0 WITH THE COORDINATES USING ASCII
 void BOARD::Fill_Cord() {
-    storage[0][0] = '!';
+    storage[0][0] = '$';
     for (int i = 1; i < columns + 1; i++) {
-        storage[0][i] = column_board;
-        column_board += 1;
+        storage[0][i] = column_board++;
     }
     for (int k = 1; k < rows + 1; k++) {
-        storage[k][0] = row_board;
-        row_board += 1;
+        storage[k][0] = row_board++;
     }
 }
 
@@ -92,15 +110,23 @@ void BOARD::Fill_Cord() {
 void BOARD::Print_Board() const {
     for (int i = 0; i < rows + 1; i++) {
         if (i != 0)
-            cout << "|";
+            text_color("|",3);
         else cout << " ";
         for (int j = 0; j < columns + 1; j++) {
-            if (storage_copy[i][j] == '#') {
+            if(i == 0 || j == 0){
+                text_color(" "+string(1,storage[i][j])+" |",3);
+            }
+            else if (storage_copy[i][j] == '#') {
                 cout << " ";
                 text_color(string(1, storage[i][j]), 4);
-                text_color(" |", 7);                                     // IF THE LETTER WAS CHANGED PRINT IT IN another colour!!!
+                text_color("  ", 7);                                     // IF THE LETTER WAS CHANGED PRINT IT IN another colour!!!
             }
-            else cout << " " << storage[i][j] << " |";
+            else if (storage[i][j] != ' '){
+                text_color(" ",15); text_color(string(1,storage[i][j]),15); text_color("  ",7);
+            }
+            else{
+                text_color(" ",7); cout << storage[i][j] << "  ";
+            }
         }
         cout << endl;
     }
@@ -119,35 +145,6 @@ void BOARD::Build_Board() {
         else if (board_input[i][1] == "V") {
             for (char it2 : board_input[i][2]) {
                 storage[init_x][init_y] = it2; init_x++; //If the word is vertical then the matrix should be incremented only in the row section
-            }
-        }
-    }
-}
-
-
-//Check if the a word has been completed
-bool BOARD::complete_word() {
-    for (int i = 1; i < board_input.size(); i++) {
-        int count = 0;
-        int init_x = board_input[i][0][0] - 64, init_y = board_input[i][0][1] - 96;
-        if (board_input[i][1] == "H") {
-            for (int j = 0; j < board_input[i][2].size(); j++) {
-                if (count == board_input[i][2].size()) {
-                    board_input[i][1] = "D";                    //Change the direction for D so that the word isn't analysed again
-                    return true;                                //and doesn't give more points than it should
-                }
-                if (storage_copy[init_x][init_y] != '#') return false; //If it is different the word hasn't been completed
-                count++; init_y++;                              //Increment count and col
-            }
-        }
-        else if (board_input[i][1] == "V") {
-            for (int j = 0; j < board_input[i][2].size(); j++) {
-                if (count == board_input[i][2].size()) {
-                    board_input[i][1] = "D";                    //Change the direction for D so that the word isn't analysed again
-                    return true;                                //and doesn't give more points than it should
-                }
-                if (storage_copy[init_x][init_y] != '#') return false;  //If it is different the word hasn't been completed
-                count++; init_x++;                              //Increment count and col
             }
         }
     }
